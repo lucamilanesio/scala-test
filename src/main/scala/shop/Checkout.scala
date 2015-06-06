d160dad5958ba4850476da99a5d009864ec67737
Luca Milanesio <luca.milanesio@gmail.com>
2015-06-07 00:41:17 +0100

    Refactor: DRY Checkout products and prices

--- src/main/scala/shop/Checkout.scala
+++ src/main/scala/shop/Checkout.scala
@@ -1,9 +1,15 @@
 package shop
 
 class Checkout {
+  val PriceApple = BigDecimal("0.60")
+  val PriceOrange = BigDecimal("0.25")
+
+  val Apple = "apple"
+  val Orange = "orange"
+
   def scan(product: String): Option[BigDecimal] = product match {
-    case "apple" => Some(BigDecimal("0.60"))
-    case "orange" => Some(BigDecimal("0.25"))
+    case Apple => Some(PriceApple)
+    case Orange => Some(PriceOrange)
     case _ => None
   }
 
@@ -11,12 +17,12 @@ class Checkout {
     products.map(scan).flatten.fold(BigDecimal("0"))(_+_) - appleDiscount(products) - orangeDiscount(products)
 
   def appleDiscount(products: Seq[String]): BigDecimal = {
-    val numFreeApples = products.count(_ == "apple") / 2
-    numFreeApples * BigDecimal("0.60")
+    val numFreeApples = products.count(_ == Apple) / 2
+    numFreeApples * PriceApple
   }
 
   def orangeDiscount(products: Seq[String]): BigDecimal = {
-    val numFreeOranges = products.count(_ == "orange") / 3
-    numFreeOranges * BigDecimal("0.25")
+    val numFreeOranges = products.count(_ == Orange) / 3
+    numFreeOranges * PriceOrange
   }
 }
