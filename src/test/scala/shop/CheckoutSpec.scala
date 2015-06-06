67d9faf361e4aa972d20e52596e42d4a2eaf01b8
Luca Milanesio <luca.milanesio@gmail.com>
2015-06-06 23:27:52 +0100

    Refactoring: making test code more compact but readable

--- src/test/scala/shop/CheckoutSpec.scala
+++ src/test/scala/shop/CheckoutSpec.scala
@@ -9,41 +9,31 @@ class CheckoutSpec extends FlatSpec with GivenWhenThen with Matchers with Option
   "A checkout" should
     "scan apples" in {
     When("scanning an apple")
-    val item = checkout scan "apple"
-
-    Then("item should be scanned")
-    item should not be (empty)
+    Then("should return an item")
+    checkout scan "apple" should not be (empty)
   }
 
   it should "scan oranges" in {
     When("scanning an orange")
-    val item = checkout scan "orange"
-
-    Then("item should be scanned")
-    item should not be (empty)
+    Then("should return an item")
+    checkout scan "orange" should not be (empty)
   }
 
   it should "not scan kiwis" in {
     When("scanning a kiwi")
-    val item = checkout scan "kiwi"
-
-    Then("item should not be found")
-    item should be (empty)
+    Then("should return nothing")
+    checkout scan "kiwi" should be (empty)
   }
 
   it should "return 60p as cost for an apple" in {
     When("scanning an apple")
-    val itemCost = checkout scan "apple"
-
     Then("should return a 60p cost")
-    itemCost.value should be ("60p")
+    (checkout scan "apple").value should be ("60p")
   }
 
   it should "return 25p as cost for an orange" in {
     When("scanning an orange")
-    val itemCost = checkout scan "orange"
-
     Then("should return a 25p cost")
-    itemCost.value should be ("25p")
+    (checkout scan "orange").value should be ("25p")
   }
 }
